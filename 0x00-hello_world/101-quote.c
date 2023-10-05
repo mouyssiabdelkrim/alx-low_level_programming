cc8410ab7f370e7ff3503789d7baab61b0ac7a41
mouyssiabdelkrim <mouyssi.abdelkrim@gmail.com>
2023-10-05 15:00:56 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,4 @@
-#include <stdio.h>
+#include <unistd.h>
 
 /**
  * main - Entry point
@@ -6,7 +6,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	char quo [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+	write(1.quo.59);
 	return (1);
 }
