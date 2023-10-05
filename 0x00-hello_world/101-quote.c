74e6c5fcfd6d8d76542c736b94166f695e4f3354
mouyssiabdelkrim <mouyssi.abdelkrim@gmail.com>
2023-10-05 15:03:24 +0100

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,11 @@
  * main - Entry point
  * Return: Always 1 (Success)
  */
+
 int main(void)
 {
-	char quo [] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
-	write(1.quo.59);
+	char quo[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
+
+	write(1,quo,59);
 	return (1);
 }
