b07ce00e6064ab1b674ea47f15b452ddfdfd11ea
mouyssiabdelkrim <mouyssi.abdelkrim@gmail.com>
2023-10-23 13:07:56 +0100

    a

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,43 +1,31 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * _strstr - prints the consecutive caracters of s1 that are in s2.
- * @haystack: source string
- * @needle: searching string
- *
- * Return: new string.
+ * _strstr - Entry point
+ * Desc: Entry
+ *@haystack: pointer
+ *@needle: pointer
+ * Return: Always 0 (Success)
  */
 char *_strstr(char *haystack, char *needle)
 {
-	while (*haystack)
+	int i;
+	int j;
+	int k;
+
+	for (i = 0 ; haystack[i] ; i++)
 	{
-		if ((*haystack == *needle && coincidence(haystack, needle) == 1) || !*needle)
+		for (k = i, j = 0 ; needle[j] != '\0' ; j++, k++)
 		{
-			return (haystack);
+			if (haystack[k] != needle[j] || haystack[k] == '\0')
+			{
+				break;
+			}
 		}
-		else
+		if (needle[j] == 0)
 		{
-			haystack++;
+			return (haystack + i);
 		}
 	}
 	return (0);
-}
-/**
- * coincidence - define if the string b is inside a.
- * @a: source string
- * @b: string to be searched
- *
- * Return: 1 if there is coincidence, otherwise 0.
- */
-int coincidence(char *a, char *b)
-{
-	while (*b && *b == *a)
-	{
-		b++;
-		a++;
-	}
-
-	if (*b == '\0')
-		return (1);
-	else
-		return (0);
 }
